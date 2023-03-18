435c241f9ed30894e427cfb14cbc085a8cfa930d
Valentin OISON <oison.valentin57@gmail.com>
2023-03-18 20:19:23 +0100

    [+] Handling Players and Creating new matches

--- ChessPairingProto/PairingManager.cs
+++ ChessPairingProto/PairingManager.cs
@@ -9,63 +9,88 @@ namespace ChessPairingProto
         {
             public string Name { get; }
             public float Score { get; set; }
-            public string[] History { set; get; }
+            public List<string> History { get; }
             public int Elo { get; }
 
             
             
-            public Player(string name, int elo, int nbRound)
+            public Player(string name, int elo)
             {
                 Name = name;
                 Score = 0;
-                History = new string[nbRound];
+                History = new List<string>();
                 Elo = elo;
             }
+
+            public void ShowPlayerHistory()
+            {
+                Console.WriteLine($"History of {Name}");
+                foreach (var match in History)
+                    Console.WriteLine($"\t{match}");
+                Console.WriteLine("----------------------");
+            }   
         }
         
-        int NbRound;
-        int CurrentRound;
         
-        List<Player> Players;
-        List<(int, Player, Player)> CurrentMatches;
+        
+        public List<Player> Players;
+        public List<(Player, Player)> CurrentMatches;
         
         private Random _rd;
+        
+        public int NbRound;
+        public int CurrentRound;
+
+        public bool RoundRunning;
 
         public PairingManager(int nbRound)
         {
             NbRound = nbRound;
+            CurrentRound = 1;
             Players = new List<Player>();
-            CurrentMatches = new List<(int,Player,Player)>();
+            CurrentMatches = new List<(Player,Player)>();
             _rd = new Random();
+            RoundRunning = false;
         }
 
-        public void AddPlayer(string name, int elo)
+        public void AddPlayer(Player player)
         {
-            Players.Add(new Player(name, elo, NbRound));
+            Players.Add(player);
         }
 
         public void CreateNewMatches()
         {
-            int idMatch = 0;
+            Console.WriteLine("\tCreating new rounds");
+            if (RoundRunning)
+            {
+                Console.WriteLine("Round still running");
+                return;
+            }
+            RoundRunning = true;
             Player? playerNotPaired = null;
             Player playerToPair;
             
-            for (float i = CurrentRound; i >= -.5f ; i -= .5f)
+            for (float i = CurrentRound-1; i >= -.5f ; i -= .5f)
             {
+                Console.WriteLine($"i = {i} ---------------------");
                 // Take all the players with i points
                 List<Player> playersWithSameScore = Players.FindAll(player => player.Score == i);
+                Console.WriteLine($"Found {playersWithSameScore.Count} with same score");
                 
                 // If there is no player with this score, skip
                 if (playersWithSameScore.Count == 0)
                 {
                     // If, at the end, there is only one player left, exempt him of his game
-                    if (CurrentRound == -.5f)
+                    if (i == -.5f && playerNotPaired != null)
                     {
-                        CurrentMatches.Add((idMatch, playerNotPaired!, new Player("EXEMPT",0,NbRound)));
-                        EnterScore(idMatch, "1-0");
+                        Console.WriteLine($"Exempting {playerNotPaired.Name} of his game");
+                        CurrentMatches.Add((playerNotPaired!, new Player("EXEMPT",0)));
+                        
+                        //Entering Score
+                        EnterScore(CurrentMatches.Count, "1-0");
                     }
-                    else
-                        continue;
+
+                    continue;
                 }
                 
                 // If a player was not paired before, pair him first
@@ -73,8 +98,7 @@ namespace ChessPairingProto
                 {
                     playerToPair = playersWithSameScore[_rd.Next(playersWithSameScore.Count)];
                     playersWithSameScore.Remove(playerToPair);
-                    CurrentMatches.Add((idMatch, playerNotPaired, playerToPair));
-                    idMatch++;
+                    CurrentMatches.Add((playerNotPaired, playerToPair));
                     playerNotPaired = null;
                 }
                 
@@ -82,11 +106,12 @@ namespace ChessPairingProto
                 if (playersWithSameScore.Count == 0)
                     continue;
                 
-                // Creates the matches
+                // Create the matches
                 // If there is an odd number of players, keep one to next step
                 if (playersWithSameScore.Count % 2 == 1)
                 {
                     playerNotPaired = playersWithSameScore[_rd.Next(playersWithSameScore.Count)];
+                    Console.WriteLine($"Keeping {playerNotPaired.Name} for next step");
                     playersWithSameScore.Remove(playerNotPaired);
                 }
                 
@@ -95,21 +120,80 @@ namespace ChessPairingProto
                 for (int j = 0; j < nbOfRoundToCreate; j++)
                 {
                     playerToPair = playersWithSameScore[_rd.Next(1, playersWithSameScore.Count)];
-                    CurrentMatches.Add((idMatch, playersWithSameScore[0], playerToPair));
+                    CurrentMatches.Add((playersWithSameScore[0], playerToPair));
+                    Console.WriteLine($"Pairing {playersWithSameScore[0].Name} with {playerToPair.Name}");
                     playersWithSameScore.RemoveAt(0);
                     playersWithSameScore.Remove(playerToPair);
                 }
             }
+            
+            Console.WriteLine();
         }
 
         public void EnterScore(int idMatch, string result)
         {
-            (int, Player, Player) currentMatch = CurrentMatches[idMatch];
-            switch ("1/2-1/2")
+            (Player, Player) currentMatch = CurrentMatches[idMatch-1];
+            switch (result)
             {
-                
+                // Adding scores
+                case "1/2-1/2":
+                    currentMatch.Item1.Score += 0.5f;
+                    currentMatch.Item2.Score += 0.5f;
+                    break;
+                case "1-0":
+                    currentMatch.Item1.Score += 1f;
+                    break;
+                case "0-1":
+                    currentMatch.Item2.Score += 1f;
+                    break;
+            }
+            
+            // Adding the result to both player's history
+            currentMatch.Item1.History.Add($"{currentMatch.Item1.Name} - {currentMatch.Item2.Name} {result}");
+            currentMatch.Item2.History.Add($"{currentMatch.Item1.Name} - {currentMatch.Item2.Name} {result}");
+            
+            // Removing game of current matches
+            CurrentMatches.RemoveAt(idMatch-1);
+            
+            // Ending Round if no more matches
+            if (CurrentMatches.Count == 0)
+            {
+                RoundRunning = false;
+                Console.WriteLine($"END OF THE ROUND {CurrentRound}\n");
+                CurrentRound++;
             }
         }
+        
+        // DEBUGGING FUNCTIONS
+        public void ShowCurrentMatches()
+        {
+            Console.WriteLine("Current matches :");
+            for (int i = 0; i < CurrentMatches.Count; i++)
+            {
+                var match = CurrentMatches[i];
+                Console.WriteLine($"\t{i+1}. {match.Item1.Name} - {match.Item2.Name}");
+            }
+            Console.WriteLine();
+        }
+
+        public void ShowAllPlayerHistory()
+        {
+            foreach (var player in Players)
+                player.ShowPlayerHistory();
+        }
+
+        public void ShowScoreBoard()
+        {
+            Console.WriteLine("Scores:");
+            for (float i = CurrentRound-1; i >= 0 ; i-=.5f)
+            {
+                var temp = Players.FindAll(player => player.Score == i);
+                foreach (var player in temp)
+                    Console.WriteLine($"\t{player.Name} : {player.Score}");
+            }
+            Console.WriteLine();
+        }
     }
+    
 }
 
--- ChessPairingProto/Program.cs
+++ ChessPairingProto/Program.cs
@@ -1,3 +1,5 @@
+using ChessPairingProto;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -18,4 +20,46 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
-app.Run();
+PairingManager pairingManager = new PairingManager(7);
+pairingManager.AddPlayer(new PairingManager.Player("Valentin", 1199));
+pairingManager.AddPlayer(new PairingManager.Player("Jules", 1199));
+pairingManager.AddPlayer(new PairingManager.Player("MVL", 1199));
+pairingManager.AddPlayer(new PairingManager.Player("Hikaru", 1199));
+pairingManager.AddPlayer(new PairingManager.Player("Magnus", 1199));
+pairingManager.AddPlayer(new PairingManager.Player("Balcer", 1199));
+pairingManager.AddPlayer(new PairingManager.Player("Baki", 1199));
+pairingManager.AddPlayer(new PairingManager.Player("Paulo", 1199));
+pairingManager.AddPlayer(new PairingManager.Player("Flavien", 1199));
+pairingManager.AddPlayer(new PairingManager.Player("Caire", 1199));
+
+pairingManager.CreateNewMatches();
+pairingManager.ShowCurrentMatches();
+pairingManager.EnterScore(1, "1-0");
+pairingManager.EnterScore(1, "1-0");
+pairingManager.EnterScore(3, "0-1");
+pairingManager.EnterScore(1, "1-0");
+pairingManager.EnterScore(1, "1/2-1/2");
+
+pairingManager.ShowScoreBoard();
+
+pairingManager.CreateNewMatches();
+pairingManager.ShowCurrentMatches();
+pairingManager.EnterScore(2, "1-0");
+pairingManager.EnterScore(4, "1-0");
+pairingManager.EnterScore(3, "0-1");
+pairingManager.EnterScore(2, "1-0");
+pairingManager.EnterScore(1, "1/2-1/2");
+
+pairingManager.ShowScoreBoard();
+
+pairingManager.CreateNewMatches();
+pairingManager.ShowCurrentMatches();
+pairingManager.EnterScore(5, "1-0");
+pairingManager.EnterScore(2, "1-0");
+pairingManager.EnterScore(1, "0-1");
+pairingManager.EnterScore(1, "1-0");
+pairingManager.EnterScore(1, "1/2-1/2");
+
+pairingManager.ShowScoreBoard();
+pairingManager.ShowAllPlayerHistory();
+//app.Run();
